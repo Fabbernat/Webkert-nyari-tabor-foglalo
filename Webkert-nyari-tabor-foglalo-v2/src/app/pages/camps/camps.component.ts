79d7d969ca7abacadb1cbd3e506af453a0f94682
Fabian Bernat <fabbernat@gmail.com>
2025-03-17 10:40:13 +0100

    yes

---
+++ Webkert-nyari-tabor-foglalo-v2/src/app/pages/camps/camps.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camps',
+  imports: [],
+  templateUrl: './camps.component.html',
+  styleUrl: './camps.component.css'
+})
+export class CampsComponent {
+
+}
