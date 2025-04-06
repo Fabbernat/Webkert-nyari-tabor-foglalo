6f15c3fb364482f73130c8a9e87537d96b349709
Fabbernat <fabbernat@gmail.com>
2025-04-06 20:11:22 +0200

    todo: fabbernat.github.io

--- Webkert-nyari-tabor-foglalo-v12/src/app/app.routes.ts
+++ Webkert-nyari-tabor-foglalo-v12/src/app/app.routes.ts
@@ -8,14 +8,40 @@ import { CampListComponent } from './features/camps/components/camp-list/camp-li
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/auth/components/register/register.component';
 import { UserProfileComponent } from './features/profile/components/user-profile/user-profile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminModule } from './features/admin/admin.module';
 
 
 export const routes: Routes = [
-    { path: 'camps', component: CampListComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'homelists', component: CampListComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'profile', component: UserProfileComponent },
-    { path: '', redirectTo: '/camps', pathMatch: 'full' },
+    { path: 'admin', component: AdminModule },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'camps', component: CampListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'hero', component: HeroComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'camp-types', component: CampTypesComponent },
+  { path: 'create-camp', component: CreateCampComponent },
+  { path: 'navigation', component: NavigationComponent },
+  { path: 'camp-details', component: CampDetailsComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'user-manager', component: UserManagerComponent },
+  { path: 'featured-camps', component: FeaturedCampsComponent },
+  { path: 'stats-dashboard', component: StatsDashboardComponent },
+  { path: 'camp-registration', component: CampRegistrationComponent },
+  { path: 'user-type-selection', component: UserTypeSelectionComponent },
+  { path: 'camp-location-manager', component: CampLocationManagerComponent },
     {
     path: '',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
@@ -25,8 +51,8 @@ export const routes: Routes = [
     loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: 'taborok',
-    loadChildren: () => import('./features/camps/camps.module').then(m => m.CampsModule)
+    path: 'home',
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'profil',
