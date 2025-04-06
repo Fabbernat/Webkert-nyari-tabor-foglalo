f8de70fd8665716f5635c4ec62de393f62b8c5d8
Fabbernat <fabbernat@gmail.com>
2025-04-06 20:11:22 +0200

    todo: fabbernat.github.io

--- Webkert-nyari-tabor-foglalo-v12/src/app/app.component.ts
+++ Webkert-nyari-tabor-foglalo-v12/src/app/app.component.ts
@@ -1,8 +1,11 @@
 // src/app/app.component.ts
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { NavigationComponent } from './core/components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +15,15 @@ import { CommonModule } from '@angular/common';
   [RouterOutlet, HomeComponent, HeroComponent, HeaderComponent, FooterComponent, CampListComponent, RegisterComponent, CampTypesComponent, CreateCampComponent, NavigationComponent, CampDetailsComponent, EditProfileComponent, UserManagerComponent, UserProfileComponent, FeaturedCampsComponent, StatsDashboardComponent, CampRegistrationComponent, UserTypeSelectionComponent, CampLocationManagerComponent],
   */
 
-  imports: [CommonModule, RouterOutlet, RouterModule, HomeComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
