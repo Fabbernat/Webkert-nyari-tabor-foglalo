505497bab873cdf4207b5c29c3a522e27c1e816f
Fabbernat <fabbernat@gmail.com>
2025-04-06 19:09:08 +0200

    v2.2 quite good

--- Webkert-nyari-tabor-foglalo-v12/src/app/app.component.ts
+++ Webkert-nyari-tabor-foglalo-v12/src/app/app.component.ts
@@ -1,19 +1,20 @@
 // src/app/app.component.ts
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
 
   /* All potential imports:
   [RouterOutlet, HomeComponent, HeroComponent, HeaderComponent, FooterComponent, CampListComponent, RegisterComponent, CampTypesComponent, CreateCampComponent, NavigationComponent, CampDetailsComponent, EditProfileComponent, UserManagerComponent, UserProfileComponent, FeaturedCampsComponent, StatsDashboardComponent, CampRegistrationComponent, UserTypeSelectionComponent, CampLocationManagerComponent],
   */
- 
-  imports: [RouterOutlet, HomeComponent],
+
+  imports: [CommonModule, RouterOutlet, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  standalone: true
 })
 
 export class AppComponent implements OnInit {
