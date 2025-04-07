b16cfee83a6de65e07ebdd56b2505f70ca4943d8
Fabbernat <fabbernat@gmail.com>
2025-04-07 12:04:54 +0200

    v3.1 very gool home page!

--- Webkert-nyari-tabor-foglalo-v12/src/app/features/home/components/featured-camps/featured-camps.component.ts
+++ Webkert-nyari-tabor-foglalo-v12/src/app/features/home/components/featured-camps/featured-camps.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Camp, CampFormat, CampType } from '../../../../shared/models/camp';
 
 @Component({
   selector: 'app-featured-camps',
+  standalone: true,
   imports: [],
   templateUrl: './featured-camps.component.html',
   styleUrl: './featured-camps.component.scss'
 })
 export class FeaturedCampsComponent {
+  featuredCamps: Camp[] = [
+    {
+      name: 'A tábor neve',
+      description: 'A tábor leírása',
+      campType: CampType.CSERKESZTABOR,
+      campFormat: CampFormat.OVERNIGHT,
+      location: 'Gödöllő',
+      startDate: new Date('2025-06-01'),
+      endDate: new Date('2025-06-07'),
+      capacity: 30,
+      price: 35000,
+      registeredParticipants: 20,
+      minAge: 12,
+      maxAge: 99,
+      organizerId: 'bestSzervezo369',
+      dailyProgram: []
 
+    }
+  ];
 }
