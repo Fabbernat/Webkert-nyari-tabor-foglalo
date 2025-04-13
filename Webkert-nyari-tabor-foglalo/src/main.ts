3d766f7cefdf6863fa3c99e9b407b5a91533e9da
Fabbernat <fabbernat@gmail.com>
2025-04-13 14:12:05 +0200

    5.2 Only 1 error in browser

--- Webkert-nyari-tabor-foglalo/src/main.ts
+++ Webkert-nyari-tabor-foglalo/src/main.ts
@@ -1,8 +1,10 @@
+// C:\VSCodeProjects\Webkert-nyari-tabor-foglalo\Webkert-nyari-tabor-foglalo\src\main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
