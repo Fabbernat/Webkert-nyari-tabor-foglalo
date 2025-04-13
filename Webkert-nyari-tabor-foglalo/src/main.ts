20577790163f866539fa8df2f43aa5e41c170507
Fabbernat <fabbernat@gmail.com>
2025-04-13 20:40:33 +0200

    update gitignore

--- Webkert-nyari-tabor-foglalo/src/main.ts
+++ Webkert-nyari-tabor-foglalo/src/main.ts
@@ -1,11 +1,7 @@
 // C:\VSCodeProjects\Webkert-nyari-tabor-foglalo\Webkert-nyari-tabor-foglalo\src\main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-  ],
-})
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
