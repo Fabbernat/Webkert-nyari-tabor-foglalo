f3c8c2ab9a5a48dada7ddfa11520efe69cc623bc
Fabbernat <fabbernat@gmail.com>
2025-04-15 09:13:23 +0200

    smoother user exprerence: add missing HttpCLient

--- Webkert-nyari-tabor-foglalo/src/app/app.config.ts
+++ Webkert-nyari-tabor-foglalo/src/app/app.config.ts
@@ -1,9 +1,13 @@
 // C:\VSCodeProjects\Webkert-nyari-tabor-foglalo\Webkert-nyari-tabor-foglalo\src\app\app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
+
