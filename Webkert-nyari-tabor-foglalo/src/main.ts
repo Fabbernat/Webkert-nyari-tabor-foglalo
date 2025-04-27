abc44a44f3468eef580ba319e7d55c0c7b6705b9
Fabbernat <fabbernat@gmail.com>
2025-04-27 13:41:01 +0200

    9.6.1 same shit different day

--- Webkert-nyari-tabor-foglalo/src/main.ts
+++ Webkert-nyari-tabor-foglalo/src/main.ts
@@ -1,9 +1,7 @@
 // C:\VSCodeProjects\Webkert-nyari-tabor-foglalo\Webkert-nyari-tabor-foglalo\src\main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HomeComponent } from './app/pages/home/home.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-provideHttpClient(); // hiányzik valahonnan (valszeg ngmodule-ban vagy az applicationconfigban) egy provideHttpClient(
+bootstrapApplication(HomeComponent, appConfig)
+  .catch(err => console.error(err)); // hiányzik valahonnan (valszeg ngmodule-ban vagy az applicationconfigban) egy provideHttpClient(
