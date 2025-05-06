e9d2425923a29a4736f4be2d628187da2fd21766
Fabian Bernat <fabbernat@gmail.com>
2025-05-07 00:18:18 +0200

    update camp-details.component.ts

--- src/app/core/features/camps/components/camp-details/camp-details.component.ts
+++ src/app/core/features/camps/components/camp-details/camp-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Camp } from '../../../../../shared/models/camp/camp.component';
+import { CampService } from '../../../../../services copy/camp.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-camp-details',
@@ -6,7 +10,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './camp-details.component.html',
   styleUrl: './camp-details.component.scss'
 })
-export class CampDetailsComponent {
+export class CampDetailsComponent implements OnInit {
   // Bemeneti adatok a szülő komponenstől
   @Input() campName!: string;
   @Input() capacity!: number;
@@ -20,6 +24,32 @@ export class CampDetailsComponent {
     name: 'Minta tábor',
     description: 'Ez egy példa leírás a táborról.'
   };
+
+  camps: Camp[] = [];
+  selectedCamp: Camp | null = null;
+  isLoading = true;
+
+  constructor(
+    private campService: CampService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    // 🔄 Táborok betöltése Firestore-ból
+    this.campService.getCamps().subscribe({
+      next: (data) => {
+        data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.snackBar.open('Hiba történt a táborok betöltésekor.', 'Bezár', {
+          duration: 3000
+        });
+        this.isLoading = false;
+      }
+    });
+  }
 
   // Foglalás kezelése
   onReserve() {
@@ -35,5 +65,19 @@ export class CampDetailsComponent {
   onViewed() {
     // ⬅️ Jelzés, hogy a felhasználó megnézte a tábort (kapacitással együtt)
     this.logView.emit(this.capacity);
+  }
+
+  // ✅ Tábor kiválasztása
+  selectCamp(camp: Camp): void {
+    this.selectedCamp = camp;
+  }
+
+  // 🗑️ Tábor törlése
+  deleteCamp(campId: string): void {
+    this.campService.deleteCamp(campId).then(() => {
+      this.snackBar.open('Tábor sikeresen törölve.', 'Bezár', {
+        duration: 3000
+      });
+    });
   }
 }
