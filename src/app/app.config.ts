1d739d1c5e5001130efb71777b61bbb21602db06
Fabian Bernat <fabbernat@gmail.com>
2025-05-08 21:19:01 +0200

    v9.5.1 added the secrets too, and still runs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,16 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
   provideRouter(routes),
+  provideFirebaseApp(() =>
+    initializeApp({
+    apiKey: "AIzaSyBSLrSsxpMDFq7_xADx57SK4B5zgkCpmoE",
+        authDomain: "webkert-nyari-tabor-foglalo.firebaseapp.com",
+        projectId: "webkert-nyari-tabor-foglalo",
+        storageBucket: "webkert-nyari-tabor-foglalo.firebasestorage.app",
+        messagingSenderId: "595746910150",
+        appId: "1:595746910150:web:e8edc2ad6fd49d4b594cc7",
+        measurementId: "G-2DV7GR8V8F"
+  })),
   provideAuth(() => getAuth()),
   provideFirestore(() => getFirestore()),
   ]
