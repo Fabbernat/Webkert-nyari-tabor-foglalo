488cdd86da25d1d774a8c62bacedc0e27524178f
Fabian Bernat <fabbernat@gmail.com>
2025-05-15 09:43:03 +0200

    It works! Gallery appears, but no css yet in home site

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { GalleryComponent } from '../../shared/gallery/gallery.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, RouterLink, CommonModule],
+  imports: [MatButtonModule, RouterLink, CommonModule, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
